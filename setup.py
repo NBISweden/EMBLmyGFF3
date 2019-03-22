66bbe15bb470392f31eff6e262ec741dc7b37bd2
Martin Norling <martin.norling@nbis.se>
2019-03-22 14:23:36 +0100

    rebase: Modify structure to work as a module
    Changes the structure and setup.py so that the script can once again
    be installed as a module and run as a command line application.

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python2.7
+"""
+Setup script for EMBLmyGFF3. This tool is developed to convert and reformat the
+common GFF3 format into the more stringent EMBL format used for data submission
+to the European Nucleotide Archive (ENA).
+"""
 
 from setuptools import setup, find_packages
 
 setup(
     name='EMBLmyGFF3',
-    version='1.2.2',
+    version='2.0.0',
 
-    description='An efficient way to convert gff3 annotation files into EMBL format ready to submit',
+    description=('An efficient way to convert gff3 annotation files into EMBL '
+                 'format ready to submission'),
 
     url='https://github.com/NBISweden/EMBLmyGFF3',
-    download_url='https://github.com/NBISweden/EMBLmyGFF3/archive/v1.2.2.tar.gz',
+    download_url='https://github.com/NBISweden/EMBLmyGFF3/archive/v2.0.0.tar.gz',
     author='Martin Norling, Niclas Jareborg, Jacques Dainat',
 
     license='GPL-3.0',
@@ -19,7 +25,8 @@ setup(
     include_package_data=True,
 
     entry_points={
-        'console_scripts': ['EMBLmyGFF3 = EMBLmyGFF3:main',
+        'console_scripts': [
+            'EMBLmyGFF3 = gffemblconverter.convert:main',
         ],
     }
 )
