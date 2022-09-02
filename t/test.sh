76148bc4859c74f83f012e82b21da696c6d168a3
Jacques Dainat <jacques.dainat@sys2diag.cnrs.fr>
2022-09-02 11:37:25 +0200

    append test to run dbxref and seq oder

--- t/test.sh
+++ t/test.sh
@@ -24,7 +24,7 @@ sed -Ei -e 's/^(DT   )[0-9]{2}-[A-Za-z]{3}-[0-9]{4}/\1'"$thedate"'/' \
 SUCCESS=0
 FAIL=0
 
-for NAME in augustus maker prokka; do
+for NAME in augustus maker prokka prokka_disorder dbxref_test ; do
     RESULT_FILE="EMBLmyGFF3-${NAME}-example.embl"
     EXPECTED_FILE="EMBLmyGFF3-${NAME}-test.embl"
     [ -f "$RESULT_FILE" ] && rm $RESULT_FILE
@@ -36,6 +36,7 @@ for NAME in augustus maker prokka; do
         diff "$RESULT_FILE" "$EXPECTED_FILE"
         FAIL=$(( $FAIL + 1 ))
     fi
+    [ -f "$RESULT_FILE" ] && rm $RESULT_FILE
 done
 
 if [ $FAIL -eq 0 ]; then
@@ -43,5 +44,5 @@ if [ $FAIL -eq 0 ]; then
     exit 0
 fi
 
-echo "Failed $FAIL out of 3 tests"
+echo "Failed $FAIL out of 5 tests"
 exit 1
